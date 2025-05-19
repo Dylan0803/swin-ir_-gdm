ad7fca6251defdeb1927bce5679033afbf5f762a
dylan0803 <14384795+dylan0803@user.noreply.gitee.com>
2025-05-19 16:19:44 +0800

    移除下采样层

--- models/network_swinir_multi.py
+++ models/network_swinir_multi.py
@@ -364,7 +364,6 @@ class BasicLayer(nn.Module):
     def __init__(self, dim, input_resolution, depth, num_heads, window_size,
                  mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., norm_layer=nn.LayerNorm, downsample=None, use_checkpoint=False):
-
         super().__init__()
         self.dim = dim
         self.input_resolution = input_resolution
@@ -374,20 +373,17 @@ class BasicLayer(nn.Module):
         # build blocks
         self.blocks = nn.ModuleList([
             SwinTransformerBlock(dim=dim, input_resolution=input_resolution,
-                                 num_heads=num_heads, window_size=window_size,
-                                 shift_size=0 if (i % 2 == 0) else window_size // 2,
-                                 mlp_ratio=mlp_ratio,
-                                 qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                 drop=drop, attn_drop=attn_drop,
-                                 drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
-                                 norm_layer=norm_layer)
+                               num_heads=num_heads, window_size=window_size,
+                               shift_size=0 if (i % 2 == 0) else window_size // 2,
+                               mlp_ratio=mlp_ratio,
+                               qkv_bias=qkv_bias, qk_scale=qk_scale,
+                               drop=drop, attn_drop=attn_drop,
+                               drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
+                               norm_layer=norm_layer)
             for i in range(depth)])
 
-        # patch merging layer
-        if downsample is not None:
-            self.downsample = downsample(input_resolution, dim=dim, norm_layer=norm_layer)
-        else:
-            self.downsample = None
+        # 移除下采样层
+        self.downsample = None
 
     def forward(self, x, x_size):
         for blk in self.blocks:
@@ -395,9 +391,7 @@ class BasicLayer(nn.Module):
                 x = checkpoint.checkpoint(blk, x, x_size)
             else:
                 x = blk(x, x_size)
-        if self.downsample is not None:
-            x = self.downsample(x)
-        return x
+        return x  # 直接返回，不进行下采样
 
     def extra_repr(self) -> str:
         return f"dim={self.dim}, input_resolution={self.input_resolution}, depth={self.depth}"
@@ -406,8 +400,6 @@ class BasicLayer(nn.Module):
         flops = 0
         for blk in self.blocks:
             flops += blk.flops()
-        if self.downsample is not None:
-            flops += self.downsample.flops()
         return flops
 
 
